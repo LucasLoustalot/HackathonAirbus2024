425369f20f372d5c0ea4fc27b9eaee638fcc8878
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-06-11 16:07:36 +0200

    UPDATE: web interface

--- interface/interface.py
+++ interface/interface.py
@@ -1,16 +1,41 @@
 import csv
-from flask import Flask, render_template, request
+import json
+from flask import Flask, render_template, request, Response, jsonify, make_response
 
 app = Flask(__name__, template_folder="pages")
 
 
-def parse_result_csv(str: str) -> list:
-    r = csv.reader([str])
-    return list(r)
+def parse_result_csv(lst: list[str]) -> list:
+    ret_lst: list = []
+
+    for s in lst:
+        r = s.split(', ')
+        ret_lst.append(r)
+    return ret_lst
 
 @app.route('/request_search', methods=['POST'])
 def get_search_results():
-    print(request.form)
+    keywords = request.get_json()['value']
+    ## Send to google search
+
+    ## get request result
+    test = ["name, location, link, contact, revenue, size, certifications, skills, main domain, main customers",
+"name, location, link, contact, revenue, size, certifications, skills, main domain, main customers"]
+
+
+    header = ["Name", "Location", "Link", "Contact", "Revenue", "Size", "Certifications", "Skills", "Main domain", "Main customers"]
+    response = parse_result_csv(test)
+    jsonResponse = {}
+    data = {}
+    y = 0
+    for r in response:
+        data.clear()
+        for i in range(0, len(r) - 1):
+            data[header[i]] = r[i]
+        jsonResponse[y] = data
+        y += 1
+    print(jsonResponse)
+    return jsonify(jsonResponse)
 
 @app.route('/')
 def homepage():
--- interface/pages/main.js
+++ interface/pages/main.js
@@ -1,6 +1,12 @@
-function main()
-{
+// function send_input()
+// {
+//     var request = new Request("request_search");
+//     request.body = document.getElementById('input_field').textContent
 
-}
+//     alert('sending data' + request.body)
 
-document.onload(main())
+//     fetch(request)
+//       .then(function (reponse) {
+//         console.log(reponse.blob());
+//       })
+// }
