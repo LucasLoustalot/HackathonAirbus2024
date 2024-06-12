d528f7d52ac419c1c90caf93c492089edd63e087
Nathanael <nathanael.epitech@gmail.com>
2024-06-12 10:32:47 +0200

    ADD: filter to search

--- research/researchKeyword.py
+++ research/researchKeyword.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import research.getURLs as getURLs
 import interface.translate
+import research.deleteDuplicates
 
-def search(keyWord : str, countryCode : str, language : str) :
+def search(keyWord : str, countryCode : str, language : str) -> list[str] :
     option = webdriver.ChromeOptions()
     option.add_argument("--headless=new")
 
@@ -17,6 +18,11 @@ def search(keyWord : str, countryCode : str, language : str) :
 
     companyName = getURLs.getAllURLs(driver.page_source)
 
-    print(companyName)
+    filteredName = research.deleteDuplicates.deleteDuplicates(companyName)
 
     driver.close()
+
+    return [
+            "Airbus; Toulouse; https://www.airbus.com/fr/airbus-atlantic; support@airbus.com; 100M; 100K; FR; Skil; Aviation; Army",
+            "name2; location2; link2; contact2; revenue2; size2; Airbus.com; skills2; main domain2; main customers2",
+        ]
