2bba7e6baf19c1ec68171f433c3a46a408a36f2b
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-06-12 14:03:03 +0200

    FIX: server sorting

--- interface/interface.py
+++ interface/interface.py
@@ -47,13 +47,12 @@ def get_search_results():
     language = languages[country_code]
 
     try:
-        #resultCSV = research.search(keywords, country_code, language)
+        resultCSV = research.search(keywords, country_code, language)
 
         resultCSV = [
             "Airbus; Toulouse; https://www.airbus.com/fr/airbus-atlantic; support@airbus.com; 100M; 100K; FR; Skil; Aviation; Army",
             "name2; location2; link2; contact2; revenue2; size2; Airbus.com; skills2; main domain2; main customers2",
         ]
-
 
         response = parse_result_csv(resultCSV)
         jsonResponse = {}
@@ -68,21 +67,16 @@ def get_search_results():
                 data[data_headers[i]] = r[i]
             jsonResponse[y] = data
             y += 1
-        print(str(lens))
         lens = dict(sorted(lens.items(), key=lambda item: item[1]))
-        print(str(lens))
-        print(str(jsonResponse))
-
         response = {}
         y = 0
-        for i in range(len(lens), 0):
-            response[y] = jsonResponse[lens.values()[i]]
+        for val in reversed(lens.keys()):
+            print("Val: " + str(val))
+            response[y] = jsonResponse[val]
             y += 1
-        print(response)
-        
         return jsonify(response)
     except Exception as e:
-        print("Error while processing: " + e)
+        print("Error while processing: " + str(e))
         return "{}"
 
 
